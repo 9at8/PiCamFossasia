c25363c87264dc88b78cd7ba75e2bf6e5cf770e2
Aditya <aditya.1998thakral@gmail.com>
2015-12-04 08:18:59 +0000

    commiting...

--- task.py
+++ task.py
@@ -1,5 +1,6 @@
 import picamera
 from time import sleep
+from subprocess import call
 
 # camera instance created from PiCamera class
 camera = picamera.PiCamera()
@@ -9,3 +10,9 @@ for i in range(10):
     sleep(2)
     # Image captured as img_i.jpg
     capture.capture('img_' + str(i) + '.jpg')
+
+call('git init', shell = True)
+call('git add .', shell = True)
+call('git commit -a "commiting..."', shell = True)
+call('git push origin master', shell = True)
+
